c0da4e7792435ae6eede16a9b00a06a8bb345d9c
ALEX\Alex <krisnaalexander@gmail.com>
2021-03-26 14:33:03 +0700

    add middleware for redirect login

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Route::get('/admin', function(){
@@ -14,25 +14,30 @@ Route::get('/admin', function(){
 		return redirect('/login');
 	}
 });
-Route::get('/admin-home', 'TempatWisataController@TempatWisataArrays');
-Route::get('/form', function () {
-    return view('tempatWisataForm');
-})->name('form');
+
+Route::group(['middleware'=>'LoginCheckMiddleware'], function(){
+	Route::post('/tempat-wisata', 'HomeController@tempatWisata')->name('tempat-wisata');
+	Route::post('/tambah-tempat-wisata', 'TempatWisataController@tambah')->name('tambah-tempat-wisata');
+	Route::get('/spots/', 'SpotController@SpotArrays')->name('spots');
+	Route::get('/tempat-wisata/{id}', 'SpotController@SpotSelects');
+	Route::get('/tempat-wisata/edit/{id}', 'TempatWisataController@getTempatWisata')->name('edit-tempat-wisata');
+	Route::post('/delete-tempat-wisata/{id}', 'TempatWisataController@destroy');
+	Route::post('/update-tempat-wisata/{id}', 'TempatWisataController@update');
+	Route::post('/tambah-spot', 'SpotController@tambah')->name('tambah-spot');
+	Route::post('/delete-spot/{id_spot}', 'SpotController@destroy');
+	Route::get('/spot/edit/{id}', 'SpotController@getSpot')->name('edit-spot');
+	Route::post('/update-spot/{id}', 'SpotController@update');
+	Route::post('/tambah-tombol','TombolController@tambahTombol')->name('tambah-tombol');
+	Route::get('/tombol/edit/{id}','TombolController@getTombol')->name('edit-tombol');
+	Route::post('/update-tombol/{id}', 'TombolController@update');
+	Route::post('/delete-tombol/{id_tombol}', 'TombolController@destroy');
+	Route::get('/admin-home', 'TempatWisataController@TempatWisataArrays');
+	Route::get('/form', function () {
+		return view('tempatWisataForm');
+	})->name('form');
+});
+
 Route::get('/login', 'AuthController@showLogin')->name('login');
 Route::post('/login', 'AuthController@login')->name('login');
 Route::get('/logout', 'AuthController@logout')->name('logout');
-Route::post('/tempat-wisata', 'HomeController@tempatWisata')->name('tempat-wisata');
-Route::post('/tambah-tempat-wisata', 'TempatWisataController@tambah')->name('tambah-tempat-wisata');
-Route::get('/spots/', 'SpotController@SpotArrays')->name('spots');
-Route::get('/tempat-wisata/{id}', 'SpotController@SpotSelects');
-Route::get('/tempat-wisata/edit/{id}', 'TempatWisataController@getTempatWisata')->name('edit-tempat-wisata');
-Route::post('/delete-tempat-wisata/{id}', 'TempatWisataController@destroy');
-Route::post('/update-tempat-wisata/{id}', 'TempatWisataController@update');
-Route::post('/tambah-spot', 'SpotController@tambah')->name('tambah-spot');
-Route::post('/delete-spot/{id_spot}', 'SpotController@destroy');
-Route::get('/spot/edit/{id}', 'SpotController@getSpot')->name('edit-spot');
-Route::post('/update-spot/{id}', 'SpotController@update');
-Route::post('/tambah-tombol','TombolController@tambahTombol')->name('tambah-tombol');
-Route::get('/tombol/edit/{id}','TombolController@getTombol')->name('edit-tombol');
-Route::post('/update-tombol/{id}', 'TombolController@update');
-Route::post('/delete-tombol/{id_tombol}', 'TombolController@destroy');
+
